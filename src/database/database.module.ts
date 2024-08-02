4d92a7d6df09e67722e3145eeb8e225b9a5ddbb5
Jignesh Mistry <jigneshkmistry>
2024-08-02 17:02:35 +0530

    fix env variable issue

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -11,11 +11,11 @@ import { ConfigModule } from '@nestjs/config';
         ConfigModule.forRoot(),
         SequelizeModule.forRoot({
             dialect: 'postgres',
-            host: process.env.host,
-            port: +process.env.port,
-            username: process.env.username,
-            password: process.env.password,
-            database: process.env.database,
+            host: process.env.host || 'movie.cr2gkqqce18e.us-east-1.rds.amazonaws.com',
+            port: +process.env.port || 5432,
+            username: process.env.username || 'postgres',
+            password: process.env.password || 'Football2024',
+            database: process.env.database || 'movie',
             autoLoadModels: true,
             synchronize: true, // Do not use this in production, use migrations instead
             ssl: true,
